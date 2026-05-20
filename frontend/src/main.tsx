a1b125d9bff793fd4933c4852cf3190c20705b1f
arescalada <arescalada@gmail.com>
2026-05-20 09:47:02 +0200

    set axios baseURL global

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,13 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import axios from 'axios'
 import './index.css'
 import App from './App.tsx'
 
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://localhost:3000'
+
 createRoot(document.getElementById('root')!).render(
-  <App />
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
